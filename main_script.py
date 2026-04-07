9fb8700bb108dac7431208b7a6330d0d67a5b5be
AnoukAmillard <anouk.amillard@esme.fr>
2026-04-08 00:08:12 +0200

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -5,3 +5,28 @@ import APP_datasets
 print(APP_datasets.avions_chaos_100)
 
 
+def policies_order (policies):
+  policies=[medical, technical issue, low on fuel, diplomatic level]
+  if medical==True:
+
+  elif:
+    pass
+  if technical issue == True:
+
+  elif:
+    pass
+      if low on fuel == True:
+
+  elif:
+    pass
+
+    for diplomatic_level in range(1,5)
+      if diplomatic level>=3 :
+
+  elif:
+    pass
+
+def sorting_algorithm(policies):
+  for i in policies:
+    if 
+  
