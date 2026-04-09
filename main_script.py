76ab813c74a7379019f81e7a12e28d484af70ccb
Karl <guinookarl@gmail.com>
2026-04-09 23:39:59 +0200

    Update main_script.py
    Had to modify all the code as this is just a prototype of what we've understood with my friends(from HighSchool) 
    
    Problem in this code : the sorting works but doesn't sort really the boolean values for medical and technical_issue

--- main_script.py
+++ main_script.py
@@ -2,8 +2,98 @@ import APP_datasets
 
 ###To use the variables inside APP_datasets : make sure to add "APP_datasets." before the variable. 
 
-print(APP_datasets.avions_chaos_100)
+#Test : print(APP_datasets.avions_chaos_100)
 
-def sorting_data(dataset):
-  policies_order=[str(input("Select the order by typing one by one the policies : fuel, medical, techinical_issue and diplomatic_level")) for i in range(4)]
-  pass
+policies_orders=[str(input("Select the order by typing one by one the policies as written : fuel, medical, techinical_issue and diplomatic_level")) for i in range(4)]
+
+
+
+def insertionSort(dataset, sort_key):
+    global duplicates, duplicates_index
+    duplicates = []
+    duplicates_index = []
+
+    for i in range(1, len(dataset)):
+        key = dataset[i]
+        j = i - 1
+        while j >= 0 and key[sort_key] < dataset[j][sort_key]:
+            dataset[j + 1] = dataset[j]
+            j -= 1
+        dataset[j + 1] = key
+
+    for i in range(len(dataset)):
+        for j in range(i + 1, len(dataset)):
+            if dataset[i] == dataset[j] and dataset[i] not in duplicates:
+                duplicates.append(dataset[i])
+                duplicates_index.append(j)
+
+    return dataset
+
+
+def selectionSort(dataset, sort_key):
+    global duplicates, duplicates_index
+    duplicates = []
+    duplicates_index = []
+
+    for i in range(len(dataset)):
+        min_idx = i
+        for j in range(i + 1, len(dataset)):
+            if dataset[j][sort_key] < dataset[min_idx][sort_key]:
+                min_idx = j
+        dataset[i], dataset[min_idx] = dataset[min_idx], dataset[i]
+
+    for i in range(len(dataset)):
+        for j in range(i + 1, len(dataset)):
+            if dataset[i] == dataset[j] and dataset[i] not in duplicates:
+                duplicates.append(dataset[i])
+                duplicates_index.append(j)
+
+    return dataset
+
+
+def sort_with_tiebreaker(dataset, sort_keys):
+    global duplicates, duplicates_index
+
+    #First insertion sort by sort_keys[0]
+    insertionSort(dataset, sort_keys[0])
+
+    #Loop through remaining keys if there are still tie-breakers
+    key_index = 1
+    while duplicates and key_index < len(sort_keys):
+        current_key = sort_keys[key_index]
+        tied_values = [d[sort_keys[key_index - 1]] for d in duplicates]
+
+        for val in tied_values:
+            group_start = None
+            group_end = None
+            for i in range(len(dataset)):
+                if dataset[i][sort_keys[key_index - 1]] == val:
+                    if group_start is None:
+                        group_start = i
+                    group_end = i
+
+            tied_group = dataset[group_start:group_end + 1]
+
+            #Alternate between insertion (even) and selection (odd)
+            if key_index % 2 == 0:
+                insertionSort(tied_group, current_key)
+            else:
+                selectionSort(tied_group, current_key)
+
+            dataset[group_start:group_end + 1] = tied_group
+
+        #Recheck for duplicates on the whole list
+        duplicates = []
+        duplicates_index = []
+        for i in range(len(dataset)):
+            for j in range(i + 1, len(dataset)):
+                if dataset[i] == dataset[j] and dataset[i] not in duplicates:
+                    duplicates.append(dataset[i])
+                    duplicates_index.append(j)
+
+        key_index += 1
+
+    if not duplicates:
+        pass
+
+    return dataset
