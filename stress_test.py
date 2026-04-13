98dce0b87f4e437218f6677642246f3f65b70c2c
Karl <guinookarl@gmail.com>
2026-04-13 09:43:07 +0200

    Update stress_test.py

--- stress_test.py
+++ stress_test.py
@@ -2,6 +2,6 @@ import APP_datasets
 import main_script
 
 main_script.policies_order() #Choose the policies order
-main_script.sort_with_tiebreaker(APP_datasets.AVIONS_INITIAL, main_script.policies_orders) #Test for 24 Planes
-main_script.sort_with_tiebreaker(APP_datasets.avions_diplomatic_50, main_script.policies_orders) #Test for 50 Planes
-main_script.sort_with_tiebreaker(APP_datasets.avions_chaos_100 , main_script.policies_orders) #Test for 100 Planes
+#print(main_script.sort_with_tiebreaker(APP_datasets.AVIONS_INITIAL, main_script.policies_orders)) #Test for 24 Planes
+#print(main_script.sort_with_tiebreaker(APP_datasets.avions_diplomatic_50, main_script.policies_orders)) #Test for 50 Planes
+#print(main_script.sort_with_tiebreaker(APP_datasets.avions_chaos_100 , main_script.policies_orders)) #Test for 100 Planes
